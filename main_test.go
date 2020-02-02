4af4b964ed5e100607e1c41f77d7c4920f0cd3cb
David Caro <david@dcaro.es>
2020-02-02 23:31:10 +0000

    [api] Added simple CRUD methods
    Added the API calls:
    * GET /note  -> get all the notes
    * GET /note/:id -> get a specific note
    * DELETE /note/:id -> delete a specific note
    * PUT /note/:id -> update a specific note (returns the updated note)
    * POST /note -> create a new note (returns the new note)
    
    It's a first approach so very simple and pending refactor.

--- main_test.go
+++ main_test.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPingRoute(t *testing.T) {
+func TestGetNoteRoute(t *testing.T) {
 	router := SetupRouter()
 
 	w := httptest.NewRecorder()
@@ -23,9 +25,40 @@ func TestPingRoute(t *testing.T) {
 		return
 	}
 	expected_note := Note{
-		Id:       "1",
 		Question: "What is the answer to the ultimate question of life, the universe and everything?",
 		Answer:   "42",
 	}
+	assert.Equal(t, expected_note, gotten_note)
+}
+
+func TestPostNoteRoute(t *testing.T) {
+	router := SetupRouter()
+	expected_question := "What is the answer to the ultimate question of life, the universe and everything?"
+	expected_answer := "42"
+	expected_note := Note{
+		Question: expected_question,
+		Answer:   expected_answer,
+	}
+	request_body := fmt.Sprintf(
+		"{'question': '%v', 'answer': '%v'}",
+		expected_note.Question,
+		expected_note.Answer,
+	)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(
+		"POST",
+		"/note/1",
+		strings.NewReader(request_body),
+	)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	var gotten_note Note
+	err := json.Unmarshal(w.Body.Bytes(), &gotten_note)
+	if err != nil {
+		return
+	}
+
 	assert.Equal(t, expected_note, gotten_note)
 }
